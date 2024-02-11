8092888998117a14eff648dab2eea75f5e41d886
John Wagenbrenner <johnwag333@gmail.com>
2024-02-11 13:47:31 -0500

    aban32

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+app.use(express.static(__dirname + '/public'))
+app.use(cors())
 
 const rappers = {
   '21 savage': {
